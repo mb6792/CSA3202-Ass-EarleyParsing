29b5429172de887bf26b2bbdb0f44898b7a6af29
Mark Bonnici <mb6792@gmail.com>
2013-01-24 11:20:49 +0100

    arranged-1st working

---
+++ CSA3202-Ass-EarleyParsing/CSA3202-Ass-EarleyParsing/com/designedbymark/Main.java
@@ -0,0 +1,13 @@
+package com.designedbymark;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainFrame mf = new MainFrame();
+		mf.setVisible(true);
+	}
+
+}
